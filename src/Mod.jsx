c9aa48ddfca227e8d2bfac6d088accdc2861834b
nikitazapekin <wotblitz362@mail.ru>
2024-01-05 23:25:36 +0300

    dragon

--- src/Mod.jsx
+++ src/Mod.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { useGLTF } from "@react-three/drei";
-
+import { Suspense } from "react";
 
 export function Mod(props) {
     const model = useGLTF("/test.glb")
 return  (
     <mesh>
         <group>
-            <primitive object={model.scene}/>
+           
+            <primitive scale={[0.5, 0.5, 0.5]} object={model.scene}/>
+           
         </group>
     </mesh>
 )
 }
+//
